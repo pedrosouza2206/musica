b7d7ef81457368e83ac684ac7bf3b6be91fc6b67
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2024-03-19 18:29:13 -0300

    exercicio pronto

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine(@"I see the crystal raindrops fall
+And the beauty of it all
+Is when the sun comes shining through
+To make those rainbows in my mind
+When I think of you sometime
+And I wanna spend some time with you");
+
+Console.ForegroundColor = ConsoleColor.Blue;
+
+Console.WriteLine(@"Just the two of us
+We can make it if we try
+Just the two of us
+(Just the two of us)
+Just the two of us
+Building castles in the sky
+Just the two of us
+You and I");
